3e179aac8e654620d821c3a4fc9b57f39335eeb2
banana <banana99ou@gmail.com>
2023-05-13 14:43:29 +0900

    new library

--- lidar.py
+++ lidar.py
@@ -1,38 +1,17 @@
-#import os
-import time
-from math import cos, sin, pi, floor
-from adafruit_rplidar import RPLidar
+from rplidar import RPLidar
+lidar = RPLidar('/dev/ttyUSB0')
 
-Dataout = open("lidar_data.txt", "w")
+info = lidar.get_info()
+print(info)
 
-SCAN_BYTE = b'\x20'
-SCAN_TYPE = 129
+health = lidar.get_health()
+print(health)
 
-PORT_NAME = "/dev/ttyUSB0"
-lidar = RPLidar(None, PORT_NAME, baudrate=115200, timeout=3)
+for i, scan in enumerate(lidar.iter_scans()):
+    print('%d: Got %d measurments' % (i, len(scan)))
+    if i > 10:
+        break
 
-max_distance = 0
-
-scan_data = [0] * 360
-
-start_time = time.time()
-try:
-    for scan in lidar.iter_scans():
-        for (_, angle, distance) in scan:
-            
-            if(time.time() - start_time) > 4 :
-                Dataout.close()
-                lidar.stop()
-                lidar.stop_motor()
-                lidar.disconnect()
-                break
-            
-            scan_data[min([359, floor(angle)])] = distance
-            Dataout.write(str(scan_data))
-            Dataout.write("\n")
-except KeyboardInterrupt:
-    print('stopping')                
 lidar.stop()
 lidar.stop_motor()
 lidar.disconnect()
-
